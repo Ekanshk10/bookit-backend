f15f793af7b39ea68c00af2674ba97a6393b824a
Ekanshk10 <156538370+Ekanshk10@users.noreply.github.com>
2025-10-28 20:27:23 +0530

    changed the output path of the prisma generate

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello");
+import  express  from "express";
+import cors from "cors"
+import "dotenv/config";
+import prisma from "./src/config/prisma.config.js"
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.listen(PORT, async()=>{
+    try{
+    await prisma.$connect();
+    console.log("DATABASE IS CONNECTED SUCCESSFULLY")
+    console.log("SERVER IS RUNNING ON PORT: ", PORT);
+
+    }catch(error){
+        console.log("ERROR IN STARTING SERVER: ", error)
+    }
+})
--- src/config/prisma.config.js
+++ src/config/prisma.config.js
@@ -1,3 +1,7 @@
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
